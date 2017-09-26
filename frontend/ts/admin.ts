4df9d4487265168c90d69b5784ca707bff97c03e
K <x00.x7f@gmail.com>
2017-09-26 23:24:15 +0900

    Modified environment for typescript

--- frontend/ts/admin.ts
+++ frontend/ts/admin.ts
@@ -1,8 +1,21 @@
-class Hello {
-    public static main() : number {
-        console.log('Hello World');
-        return 0;
+class Validator {
+    public static CheckPassword() : void {
+        var password = <HTMLInputElement>document.getElementById("password")
+        var verify_password = <HTMLInputElement>document.getElementById("verify-password")
+
+        if (password.value != verify_password.value) {
+            verify_password.setCustomValidity("Passwords Don't Match");
+        } else {
+            verify_password.setCustomValidity('');
+        }
     }
 }
 
-Hello.main();
+$(document).ready(function() {
+    $('#password').keyup(function() {
+        Validator.CheckPassword()
+    })
+    $('#verify-password').keyup(function() {
+        Validator.CheckPassword()
+    })
+})
