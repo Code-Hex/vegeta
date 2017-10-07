ad4b6fe8847f3469d35ede1ef9a33df41e213d47
K <x00.x7f@gmail.com>
2017-10-07 22:25:52 +0900

    Added to render function

--- frontend/ts/mypage.ts
+++ frontend/ts/mypage.ts
@@ -1,7 +1,9 @@
 import * as request from 'superagent';
+import * as c3 from 'c3';
 
 class Render {
     private _token: string = ""
+    private _datePtn = /^([0-9]{4}-[0-9]{2}-[0-9]{2})T([0-9]{2}:[0-9]{2}:[0-9]{2})\+[0-9]{2}:[0-9]{2}$/
     constructor() {
         let e = <HTMLInputElement>document.getElementById('api-token')
         this._token = e.value
@@ -12,10 +14,11 @@ class Render {
     }
 
     public GenerateGraph(id: Number): void {
-        this.getdata(id)
+        this.render(id)
     }
 
-    private getdata(id: Number): void {
+    private render(id: Number): void {
+        let self = this
         request.post('/mypage/api/data')
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${ this._token }`)
@@ -27,7 +30,16 @@ class Render {
                 let json = res.body
                 if (json.is_success) {
                     console.log(json.data)
-                    //window.location.reload(true)
+                    let ary = json.data as Array<any>
+                    let data = ary.map((e) => {
+                        let a = JSON.parse(e.payload)
+                        let t: string = e.updated_at
+                        a.date = t.replace(self._datePtn, "$1 $2")
+                        return a
+                    })
+
+                    let vals = Object.keys(data[data.length - 1])
+                    self.renderGraph(data, vals)
                 } else {
                     alert(`データの取得に失敗しました: ${ json.reason }`)
                     window.location.reload(true)
@@ -35,12 +47,44 @@ class Render {
             }
         })
     }
+
+    private renderGraph(jsonAry: Array<any>, values: any): void {
+        var chart = c3.generate({
+            data: {
+                x: 'date',
+                xFormat: '%Y-%m-%d %H:%M:%S', // 'xFormat' can be used as custom format of 'x'
+                json: jsonAry,
+                keys: {
+                    x: 'date', // it's possible to specify 'x' when category axis
+                    value: values,
+                }
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y-%m-%d'
+                    }
+                }
+            }
+        })        
+    }
 }
 
 var render = new Render()
 
 var actions = <NodeListOf<HTMLElement>>document.querySelectorAll('.action')
 for (let i = 0; i < actions.length; ++i) {
+    if (i == 0) {
+        let elem = actions[i]
+        elem.getAttribute("name")
+        if (name == null) {
+            console.log("id is null")
+        }
+        let id = Number(name)
+        render.GenerateGraph(id)
+    }
+    
     actions[i].addEventListener('click', function(e) {
         e.preventDefault()
         let name = this.getAttribute("name")
@@ -50,41 +94,8 @@ for (let i = 0; i < actions.length; ++i) {
         }
         let id = Number(name)
         render.GenerateGraph(id)
-        console.log(id)
     })
 }
 
-/*
-var chart = c3.generate({
-    data: {
-        x: 'date',
-        xFormat: '%Y/%m/%d', // 'xFormat' can be used as custom format of 'x'
-        json: [
-            {date: '2015/10/30', upload: 0.200, download: 0.200, total: 0.400},
-            {date: '2015/10/31', upload: 0.100, download: 0.300, total: 0.400},
-            {date: '2015/11/01', upload: 0.300, download: 0.200, total: 0.500},
-            {date: '2015/11/03', upload: 0.400, download: 0.100, total: 0.500},
-            {date: '2015/11/04', upload: 0.300, download: 0.200, total: 0.500},
-            {date: '2015/11/05', upload: 0.400, download: 0.100, total: 0.500},
-            {date: '2015/11/06', upload: 0.300, download: 0.200, total: 0.500},
-            {date: '2015/11/07', upload: 0.400, download: 0.200, total: 0.600},
-            {date: '2015/11/09', upload: 0.300, download: 0.100, total: 0.400},
-            {date: '2015/11/12', upload: 0.400, download: 0.100, total: 0.500},
-            {date: '2015/11/14', upload: 0.300, download: 0.200, total: 0.500},
-            //{date: '2015/11/15', upload: 400, download: 0, total: 400}
-        ],
-        keys: {
-            x: 'date', // it's possible to specify 'x' when category axis
-            value: ['upload', 'download', 'total']
-        }
-    },
-    axis: {
-        x: {
-            type: 'timeseries',
-            tick: {
-                format: '%Y-%m-%d'
-            }
-        }
-    }
-})
-*/
+
+
